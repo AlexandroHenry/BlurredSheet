3f7dc972146db83ecf15fe29c204d5f51f425792
alaxhenry <alaxhenry0121@gmail.com>
2022-12-14 09:53:12 +0900

    Initial Commit

---
+++ BlurredSheet/BlurredSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurredSheetApp.swift
+//  BlurredSheet
+//
+//  Created by Seungchul Ha on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct BlurredSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
